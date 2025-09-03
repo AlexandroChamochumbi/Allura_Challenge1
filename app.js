f9ff8d8070f968b297e9a02c64d0fec3a745c66f
AlexandroChamochumbi <alexandro.chamochumb@utec.edu.pe>
2025-09-03 18:00:31 -0500

    Agregar amigo implementado

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo(){
+    //leer amigo
+    let amigo = document.getElementById("amigo");
+
+    //validar amigo
+    if (amigo.value === ""){
+        alert("El campo no puede estar vacío");
+    }
+
+    //agregar amigo
+    else{
+        amigos.push(amigo.value);
+        console.log(amigos);
+        //limpiar input
+        amigo.value = "";
+    }
+    }
